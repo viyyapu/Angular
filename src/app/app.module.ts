630993d5a3d9fa0fcc40929bb41d1aba18ed6f76
Viyyapu Sirisha <viyyapu.sirisha@ltts.com>
2022-08-18 15:14:58 +0530

    commiting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherWidgetMainComponent } from './Components/weather-widget-main/weather-widget-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherWidgetMainComponent
   ],
   imports: [
     BrowserModule,
